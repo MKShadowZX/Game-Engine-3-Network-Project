aa3274ea62020448f3bc0df2de1922a689f54559
Imawaffle <cchang61@my.centennialcollege.ca>
2021-11-28 23:54:11 -0500

    Update CarIndex.cs

--- Week 1/Assets/Scripts/CarIndex.cs
+++ Week 1/Assets/Scripts/CarIndex.cs
@@ -6,6 +6,20 @@ public class CarIndex : MonoBehaviour
 {
     public int index;
 
+    //Lazy Singleton
+    #region LAZY_SINGLETON_AND_AWAKE
+    public static CarIndex instance = null;
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    #endregion
+
     private void Start()
     {
         DontDestroyOnLoad(gameObject);
